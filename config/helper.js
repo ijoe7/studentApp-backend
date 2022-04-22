8d5e8d0d4c7988e507ad7319e831413b7cb822b5
ijoe7 <iyandaoluwasegun7@gmail.com>
2022-04-22 17:29:01 +0100

    validateParams implementation and edit

--- config/helper.js
+++ config/helper.js
@@ -1,10 +1,14 @@
-const AppError = require("./appError");
 const _ = require("underscore");
 
+// Validate the parameters passed into the request.body
 exports.validateParams = (req, next, request) => {
   request.map((item) => {
-    if (!req.body[item]) return next(new AppError(`${item} is required`, 400));
+    if (!req.body[item]) return next(new Error(`${item} is required`, 400));
+    // if (typeof req.body[item] === "string") {
+    //   req.body[item] = req.body[item].toLowerCase();
+    // }
   });
+  // Filter the data in req.body to include only the items that are in the request
   let data = _.pick(req.body, request);
   return data;
 };
